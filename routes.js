001057c40e15825c7589bc0494992e1a591f1dfb
Vincent Abolarin <vincentabolarin@gmail.com>
2021-06-27 22:12:20 +0100

    add basic routes

--- routes.js
+++ routes.js
@@ -5,3 +5,23 @@ const port = process.env.PORT || 3000;
 
 // Listen for requests
 app.listen(port);
+
+// Get homepage
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', {root: __dirname});
+});
+
+// Get signup page
+app.get('/signup', (req, res) => {
+    res.sendFile('./signup.html', {root: __dirname});
+});
+
+// Get signin page
+app.get('/signin', (req, res) => {
+    res.sendFile('./signin.html', {root: __dirname});
+});
+
+// Use 404 page for invalid requests
+app.use((req, res) => {
+    res.statusCode(404).sendFile('./404.html', {root: __dirname});
+});
