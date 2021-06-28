eace1bf895f552f9c8045c032c38d96923008a94
Vincent Abolarin <vincentabolarin@gmail.com>
2021-06-29 00:05:04 +0100

    update with more routes

--- routes.js
+++ routes.js
@@ -6,22 +6,57 @@ const port = process.env.PORT || 3000;
 // Listen for requests
 app.listen(port);
 
-// Get homepage
+// Get Homepage
 app.get('/', (req, res) => {
     res.sendFile('./index.html', {root: __dirname});
 });
 
-// Get signup page
+// Get Signup page
 app.get('/signup', (req, res) => {
     res.sendFile('./signup.html', {root: __dirname});
 });
 
-// Get signin page
+// Get Signin page
 app.get('/signin', (req, res) => {
     res.sendFile('./signin.html', {root: __dirname});
 });
 
+// Get My Account page
+app.get('/account', (req, res) => {
+    res.sendFile('./account.html', {root: __dirname});
+});
+
+// Get Products page
+app.get('/products', (req, res) => {
+    res.sendFile('./products.html', {root: __dirname});
+});
+
+// Get Ratings and Reviews page
+app.get('/reviews', (req, res) => {
+    res.sendFile('./reviews.html', {root: __dirname});
+})
+
+// Get About page
+app.get('/about', (req, res) => {
+    res.sendFile('./about.html', {root: __dirname});
+});
+
+// Get Contact page
+app.get('/contact', (req, res) => {
+    res.sendFile('./contact.html', {root: __dirname});
+});
+
+// Get Help page
+app.get('/help', (req, res) => {
+    res.sendFile('./help.html', {root: __dirname});
+});
+
+// Get Our Locations page
+app.get('/locations', (req, res) => {
+    res.sendFile('./locations.html', {root: __dirname});
+});
+
 // Use 404 page for invalid requests
 app.use((req, res) => {
-    res.statusCode(404).sendFile('./404.html', {root: __dirname});
+    res.status(404).sendFile('./404.html', {root: __dirname});
 });
